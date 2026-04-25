acc88fd67c344380bc7b4371ca6bb787278d9575
aramzi296 <ahmad.ramzi@gmail.com>
2026-04-25 23:19:21 +0700

    docs: explicitly add favicon to metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,7 +16,11 @@ export const metadata: Metadata = {
   title: "Batam D'Sarana Travel | Rental Mobil, Bus & Paket Wisata Batam",
   description: "Layanan transportasi terpercaya di Batam. Menyediakan rental mobil, bus, paket wisata, penginapan hotel, dan tur kuliner terbaik di Kota Batam.",
   keywords: "dsarana.com, rental mobil batam, sewa bus batam, paket wisata batam, tour kuliner batam, hotel batam, d sarana travel",
+  icons: {
+    icon: "/icon.png",
+  },
 };
+
 
 
 export default function RootLayout({
