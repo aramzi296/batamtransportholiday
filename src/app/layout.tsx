38de3921c0189d06127bac0774569dbc8fc3b137
aramzi296 <ahmad.ramzi@gmail.com>
2026-05-18 16:33:21 +0700

    gtm

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Outfit, Inter } from "next/font/google";
 import "./globals.css";
+import Script from "next/script";
 
 const outfit = Outfit({
   variable: "--font-outfit",
@@ -33,6 +34,23 @@ export default function RootLayout({
   return (
     <html lang="id" className={`${outfit.variable} ${inter.variable} scroll-smooth`}>
       <body className="font-inter antialiased bg-white text-slate-900">
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-KDBXK735"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          />
+        </noscript>
+        <Script id="google-tag-manager" strategy="afterInteractive">
+          {`
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-KDBXK735');
+          `}
+        </Script>
         <SettingsProvider>
           <LanguageProvider>
             {children}
@@ -42,4 +60,5 @@ export default function RootLayout({
     </html>
   );
 }
+
 
